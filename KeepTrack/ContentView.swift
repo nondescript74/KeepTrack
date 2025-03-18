86ae1f8a7283d54a44790aa20b387d6564a8090b
Zahirudeen Premji <headydiscy@gmail.com>
2025-03-18 16:20:27 -0600

    Initial Commit

---
+++ KeepTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KeepTrack
+//
+//  Created by Zahirudeen Premji on 3/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KeepTrack/KeepTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeepTrackApp.swift
+//  KeepTrack
+//
+//  Created by Zahirudeen Premji on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct KeepTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
