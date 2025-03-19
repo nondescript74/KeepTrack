db62f739da15b9610881bb8afd34f410bc7fae8f
Zahirudeen Premji <headydiscy@gmail.com>
2025-03-19 09:36:34 -0600

    with remote in place and workflow created

--- KeepTrack/File.swift
+++ KeepTrack/File.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+import OSLog
+
+@Observable class WaterHistory {
+    let logger = Logger(subsystem: Bundle.main.bundleIdentifier ?? "KeepTrack", category: "WaterHistory")
+    var water: Int = 0
+    
+    func addWater(_ amount: Int) {
+        water += amount
+        logger.info("Added \(amount) units of water")
+    }
+    
+    func removeWater(_ amount: Int) {
+        if water < amount {
+            logger.error( "Not enough water")
+            return
+        }
+        logger.info( "Removed \(amount) units of water")
+        water -= amount
+    }
+}
