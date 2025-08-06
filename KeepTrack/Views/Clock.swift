39e848b294b8528e9594195f6066742e05a5e088
Zahirudeen Premji <headydiscy@gmail.com>
2025-08-06 16:11:17 -0600

    Layout to take advantage of screenspace

--- KeepTrack/Views/Clock.swift
+++ KeepTrack/Views/Clock.swift
@@ -33,22 +33,23 @@ struct Clock: View {
     @State private var colorGreen: Bool
     
     fileprivate let angleCorrection: Int = 180
-    fileprivate let clockDiameter: CGFloat = 25
-    fileprivate let clockInsideDiameter: CGFloat = 2
+    fileprivate let clockDiameter: CGFloat = 50
+    fileprivate let clockInsideDiameter: CGFloat = 5
     
     fileprivate let minuteHandWidth: CGFloat = 2
     
-    fileprivate let indicatorsWidth: CGFloat = 3
-    
-    fileprivate let hourHandLength: CGFloat = 12
-    fileprivate let letterOffset: CGFloat = 5
-    
+    fileprivate let indicatorsWidth: CGFloat = 5
     
     var body: some View {
         ZStack {
             Circle()
-                .fill(colorGreen ? Color.green.opacity(0.6) : Color.gray.opacity(0.3))
+                .fill(colorGreen ? Color.green.opacity(0.6) : Color.white)
                 .frame(width: clockDiameter, height: clockDiameter)
+            
+            Circle()
+                .stroke(style: StrokeStyle(lineWidth: 1, lineCap: .round, lineJoin: .round))
+                .frame(width: clockDiameter, height: clockDiameter)
+                .foregroundStyle(Color.gray)
             
             Circle()
                 .fill(Color.black)
@@ -56,13 +57,13 @@ struct Clock: View {
             
             Rectangle()
                 .fill(Color.red)
-                .frame(width: minuteHandWidth, height: clockDiameter - 18)
+                .frame(width: minuteHandWidth, height: clockDiameter / 2 )
                 .offset(y: clockDiameter / 3 )
                 .rotationEffect(Angle(degrees: Double(6 * minute + angleCorrection)))
             
             Rectangle()
                 .fill(Color.black)
-                .frame(width: minuteHandWidth + 2, height: clockDiameter - 20)
+                .frame(width: minuteHandWidth + 2, height: clockDiameter / 2)
                 .offset(y: clockDiameter / 3)
                 .rotationEffect(Angle(degrees: Double(30 * hour + angleCorrection)))
             
@@ -75,8 +76,8 @@ struct Clock: View {
             }
             
             Text(isAM ? "AM" : "PM")
-                .font(.system(size: 6))
-                .offset(y: letterOffset)
+                .font(.system(size: 10))
+                .offset(y: clockDiameter / 2 + 6)
         }
         .padding(5)
     }
