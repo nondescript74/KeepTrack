e77a49a8afa30d614cf506421572d05fcbbf1c3f
Zahirudeen Premji <headydiscy@gmail.com>
2025-09-24 08:31:04 -0600

    Major revision to remove AppClip as was stuck in a loop trying to resolve error in build

--- KeepTrackClip/KeepTrackClipApp.swift
+++ KeepTrackClip/KeepTrackClipApp.swift
@@ -1,17 +1,1 @@
-//
-//  KeepTrackClipApp.swift
-//  KeepTrackClip
-//
-//  Created by Zahirudeen Premji on 3/27/25.
-//
 
-import SwiftUI
-
-@main
-struct KeepTrackClipApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
